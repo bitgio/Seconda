cffc3964c3d658cdc707846a9163b1d51f21c6ba
utente <bto@hotmail.it>
2023-02-25 12:30:21 +0100

    Ce l'abbiamo fatta

--- Ass1/ass1.py
+++ Ass1/ass1.py
@@ -6,45 +6,32 @@ import time
 
 t_initial = time.time()
 object = argparse.ArgumentParser()
-object.add_argument("file", type = argparse.FileType('r', encoding= 'UTF-8'), help = "This will be the book for the counting")
+object.add_argument("file", type = argparse.FileType('r', encoding= 'UTF-8-sig'), help = "This will be the book for the counting")
 args = object.parse_args()
-#print(args.file.readlines())
 
-il_file = args.file.readlines()
+il_file = args.file.read().replace('\n', '').replace(' ', '')
+il_file = il_file.translate(str.maketrans('', '', string.punctuation))
+
 count = {}
 alf = list(string.ascii_lowercase)
+
 for letter in alf:
     count[letter] = 0
 
-c = 0
-#print(count)
+counter = 0
     
-for i in range(len(il_file)):
-    #print(line)
-    il_file[i] = il_file[i].lower()
-    il_file[i] = il_file[i].replace('\n', '')
-    il_file[i] = il_file[i].lstrip()
-    for element in il_file[i]:
-        for letter in alf:
-            if element == letter:
-                c += 1
-            count[letter] += c
-            c = 0
-
-print(count)
-all_letters = 0
-for val in count.values():
-    all_letters += val
-
-for letter in count:
-    count[letter] = (count[letter] / all_letters) * 100
+for letter in il_file:
+    if letter.lower() in alf:
+        count[letter.lower()] += 1
+        counter += 1
+    
+count = {k: ((v / counter) * 100) for k, v in count.items()} 
 
 t_final = time.time()
 elapsed_time = t_final - t_initial
 print(f'The program lasts {elapsed_time}s to run.')
 
-#print(count)
-#print(all_letters)
+print(count)
 
 plt.hist(count.values())
 plt.show()

8ee80aeb2fa918754b5ca8e3c9711a394a6b6906
utente <bto@hotmail.it>
2023-03-11 12:27:26 +0100

    Forse lo termineremo lunedì

--- Ass4/ass4.py
+++ Ass4/ass4.py
@@ -6,13 +6,25 @@ import matplotlib.pyplot as plt
 class VoltageData:
         
     def __init__(self, x = [0, 0], y = [0, 0], e = [0, 0]):
-        self.times = np.array(x, dtype = np.float64)
-        self.voltages = np.array(y, dtype = np.float64)
-        if len(self.times) == len(e):
-            self.errors = np.array(e, dtype = np.float64)
+        self._times = np.array(x, dtype = np.float64)
+        self._voltages = np.array(y, dtype = np.float64)
+        if len(e) == len(self._times):
+            self._errors = np.array(e, dtype = np.float64)
         else:
-            self.errors = np.zeros(len(self.times))
-        self._values = np.stack((self.times, self.voltages, self.errors), axis = 1)
+            self._errors = np.zeros(len(self._times))
+        self._values = np.stack((self._times, self._voltages, self._errors), axis = 1)
+    
+    @property
+    def times(self):
+        return self._times
+    
+    @property
+    def voltages(self):
+        return self._voltages
+    
+    @property
+    def errors(self):
+        return self._errors
     
     @classmethod  
     def parse_line(cls, path):
@@ -29,7 +41,7 @@ class VoltageData:
         return iter(self._values)
     
     def __call__(self, time, order):
-        spline = spl(self.times, self.voltages, k = order)
+        spline = spl(self._times, self._voltages, k = order)
         return spline(time)
     
     def __repr__(self):
@@ -42,15 +54,16 @@ class VoltageData:
         for value in self._values:
             print(value)
             
-    '''def plot(self, ax, rangex, rangey, colour):
-        fig, bx = plt.subplots(1, ax)
-        bx.set_xlim(rangex[0], rangex[1])
-        bx.set_ylim(rangey[0], rangey[1])
-        bx.set_xlabel('times')
-        bx.set_ylabel('voltages')
-        bx.plot(self.times, self.voltages, colour)'''
-        
+    def plot(self, ax = None, tit = '', rangex = [None, None], rangey = [None, None], colour = None):
+        plt.axes(arg = ax, xlabel = 'times', title = tit, xlim = (rangex[0], rangex[1]), ylabel = 'voltages', ylim = (rangey[0], rangey[1]))
+        plt.errorbar(self._times, self._voltages, yerr = self._errors, ecolor = colour, fmt = 'o')
+        plt.show()        
+
 
 if __name__ == '__main__':
-    volt = VoltageData.parse_line('sample_data_file_with_errs.txt')
-    print(volt(5., 3))
+    #x = np.array([1., 2., 3., 4., 5.])
+    #y = np.random.rand(5)
+    #v = VoltageData(x, y)
+    v = VoltageData.parse_line('sample_data_file_with_errs.txt')
+    print(v(5., 3))
+    v.plot(tit = 'Prova')
